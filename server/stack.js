7b472fd86ce9ed5839522058aff027ec4210f6a0
sebastian jonasson <sebastianjonasson@gmail.com>
2015-02-03 17:11:35 +0100

    Removed some unused urls

--- server/stack.js
+++ server/stack.js
@@ -30,16 +30,3 @@ app.listen(3000);
 
 app.use('/client', express.static('../front'));
 app.use('/bower_components', express.static('../bower_components'));
-
-//stackoverflow
-//https://stackexchange.com/oauth?client_id=3997&scope=no_expiry,private_info&redirect_uri=http://localhost:3000/code
-
-//github
-//https://github.com/login/oauth/authorize?scope=user:email&client_id=1621eab2d7f37ed98b77
-
-//linkedin
-//https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=75zns7c2k18vb7&scope=r_fullprofile&state=asdf1337lolWTF&redirect_uri=http://localhost:3000/linkedin/auth
-
-
-
-
